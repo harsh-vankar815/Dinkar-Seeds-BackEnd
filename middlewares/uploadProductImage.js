fb6d5a3c9e88e0497a95d24cdff9b2f29da0fcf9
Harsh Vankar <cursedpower4@gmail.com>
2026-02-06 16:13:40 +0530

    Image upload automatic folder creation created if not exists

--- middlewares/uploadProductImage.js
+++ middlewares/uploadProductImage.js
@@ -1,9 +1,18 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/products");
+    // 1. Root directory se absolute path banayein
+    const rootDir = path.join(__dirname,'..', "uploads/products");
+
+    // 2. Agar folder nahi hai, to create karein (recursive true nested folders ke liye)
+    if (!fs.existsSync(rootDir)) {
+      fs.mkdirSync(rootDir, { recursive: true });
+    }
+
+    cb(null, rootDir);
   },
   filename: (req, file, cb) => {
     cb(null, `product-${Date.now()}${path.extname(file.originalname)}`);
--- middlewares/uploadProfileImage.js
+++ middlewares/uploadProfileImage.js
@@ -1,9 +1,18 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/profile");
+    // 1. Root directory se absolute path banayein
+    const rootDir = path.join(__dirname, "..", "uploads/profile");
+
+    // 2. Agar folder nahi hai, to create karein (recursive true nested folders ke liye)
+    if (!fs.existsSync(rootDir)) {
+      fs.mkdirSync(rootDir, { recursive: true });
+    }
+
+    cb(null, rootDir);
   },
   filename: (req, file, cb) => {
     cb(null, `${req.user._id}-${Date.now()}${path.extname(file.originalname)}`);
